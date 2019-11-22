3a25f94b938b1c789f2d722267aac1cf101f7277
SoxTM <58088497+SoxTM@users.noreply.github.com>
2019-11-22 14:27:21 -0500

    Add files via upload

---
+++ typeracer.py
@@ -0,0 +1,59 @@
+import re, time
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from pynput.keyboard import Key, Controller
+
+# Change these if you are playing against a friend
+friendly = False
+friendly_url = ""
+
+if friendly:
+	url = friendly_url
+else:
+	url = "http://play.typeracer.com/"
+
+string = ""
+keyboard = Controller()
+
+# Load up the page..
+driver = webdriver.Firefox()
+driver.get(url)
+
+# Wait for the page to load
+time.sleep(5)
+
+# Press shortcut for new race
+keyboard.press(Key.ctrl)
+keyboard.press(Key.alt)
+
+if friendly:
+	keyboard.press('k')
+else:
+	keyboard.press('i')
+
+# Release pressed keys
+keyboard.release(Key.ctrl)
+keyboard.release(Key.alt)
+
+if friendly:
+	keyboard.release('k')
+else:
+	keyboard.release('i')
+
+# Wait for the race to start
+time.sleep(15)
+
+# Read the html
+html = driver.page_source
+soup = BeautifulSoup(html)
+
+# Find the text within the html
+for span in soup.find_all("span", unselectable="on"):
+    string += span.text
+
+# Loop through typing each character at a time with a delay
+for char in string:
+	keyboard.press(char)
+	keyboard.release(char)
+	time.sleep(0.018)
+
